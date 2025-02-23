eecc1417ce9eace6a379fa509619551fcb9f8b01
Luca Zavarella <lucazavarella@outlook.com>
2025-02-23 17:20:08 +0100

    baseline

---
+++ 2025/DataSaturday066Pordenone/03-configure-mlmodels-database.sql
@@ -0,0 +1,220 @@
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'MLModels')
+BEGIN
+	CREATE DATABASE [MLModels];
+END
+ELSE
+BEGIN
+	PRINT 'Database MLModels already exists.';
+END
+GO
+
+
+USE [MLModels];
+GO
+
+
+IF OBJECT_ID('dbo.models', 'U') IS NULL
+BEGIN
+	CREATE TABLE models (
+		model_name		VARCHAR(100)	NOT NULL,
+		model_version	INT				NOT NULL DEFAULT 1,
+		model			VARBINARY(MAX)	NOT NULL
+	);
+END
+
+
+IF NOT EXISTS (SELECT * FROM sys.external_languages WHERE [language] = 'py310')
+BEGIN
+	CREATE EXTERNAL LANGUAGE [py310]
+	FROM (CONTENT = N'C:\LanguageExtensions\python-lang-extension-windows-release.zip', 
+		FILE_NAME = 'pythonextension.dll', 
+		ENVIRONMENT_VARIABLES = N'{"PYTHONHOME": "C:\\Program Files\\Python310"}');
+END
+GO
+
+
+
+
+-- Let's try to call an sp_execute_external_script within a user-defined function
+IF OBJECT_ID (N'dbo.fn_test_call_external_script', N'FN') IS NOT NULL
+    DROP FUNCTION dbo.fn_test_call_external_script;
+GO
+CREATE FUNCTION dbo.fn_test_call_external_script(@test_id int)
+RETURNS int
+AS
+BEGIN
+    DECLARE @ret int = @test_id;
+    
+	EXEC sp_execute_external_script
+		@language =N'py310',
+		@script=N'
+import sys
+print(sys.path)
+print(sys.version)
+print(sys.executable)';EXEC sp_execute_external_script
+@language =N'py310',
+@script=N'
+import sys
+print(sys.path)
+print(sys.version)
+print(sys.executable)';
+
+    RETURN @ret;
+END;
+GO
+
+SELECT id = dbo.fn_test_call_external_script(1);
+-- ERROR! Only functions and some extended stored procedures can be executed from within a function.
+
+
+
+
+
+IF OBJECT_ID('dbo.stp_generate_binary_model', 'P') IS NOT NULL
+DROP PROCEDURE dbo.stp_generate_binary_model;
+GO
+
+CREATE PROCEDURE dbo.stp_generate_binary_model
+	@model_file_name NVARCHAR(200) = N'titanic-best-model-flaml.pkl',
+	@model_bin VARBINARY(MAX) OUTPUT
+AS
+BEGIN
+
+	DECLARE @python_script AS NVARCHAR(MAX) = N'
+import pickle
+import os
+
+main_path = r''C:\MLModels''
+
+with open(os.path.join(main_path, r''' + @model_file_name + '''), ''rb'') as f:
+	model = pickle.load(f)
+
+model_bin = pickle.dumps(model)
+'
+
+	EXECUTE sp_execute_external_script @language = N'py310'
+			, @script = @python_script 
+	, @params = N'@model_bin varbinary(max) OUTPUT'
+	, @model_bin = @model_bin OUTPUT;
+
+END
+
+GO
+
+DECLARE @model varbinary(max);
+EXECUTE stp_generate_binary_model @model_bin = @model OUTPUT;
+SELECT model = @model
+GO
+
+
+DELETE FROM dbo.models
+WHERE
+	model_name = 'titanic_flaml'
+	AND model_version = 1;
+
+DECLARE @model varbinary(max);
+EXECUTE stp_generate_binary_model @model_bin = @model OUTPUT;
+
+INSERT INTO dbo.models (model_name, model_version, model)
+VALUES (
+	  'titanic_flaml'
+	, 1
+	, @model
+);
+GO
+
+SELECT *
+FROM dbo.models;
+GO
+
+
+
+IF OBJECT_ID('dbo.stp_predict_titanic_survivors', 'P') IS NOT NULL
+DROP PROCEDURE dbo.stp_predict_titanic_survivors;
+GO
+
+CREATE PROCEDURE stp_predict_titanic_survivors
+	  @ModelName	AS VARCHAR(100)
+	, @ModelVersion	AS INT
+
+	, @Age			AS INT
+	, @Embarked		AS FLOAT
+	, @Fare			AS FLOAT
+	, @Parch		AS FLOAT
+	, @Pclass		AS INT
+	, @Sex			AS INT
+	, @SibSp		AS FLOAT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE
+		@binary_model VARBINARY(MAX),
+		@input_query NVARCHAR(MAX);
+
+	-- Build the input query to get all the input values for the model
+	SET @input_query = CONCAT(N'
+			SELECT
+			  Age		= ', @Age, N'
+			, Embarked	= CAST(', @Embarked, N' AS FLOAT)
+			, Fare		= CAST(', @Fare, N' AS FLOAT)
+			, Parch		= CAST(', @Parch, N' AS FLOAT)
+			, Pclass	= ', @Pclass, N'
+			, Sex		= ', @Sex, N'
+			, SibSp		= CAST(', @SibSp, N' AS FLOAT)')
+	
+	-- Select the model to use for scoring by name and version
+	SELECT TOP 1
+		@binary_model = model
+	FROM dbo.models
+	WHERE
+		model_name = @ModelName
+		AND model_version = @ModelVersion;
+
+	-- Get the output score for the input values using the model
+	EXECUTE sp_execute_external_script @language = N'py310'
+			, @script = N'
+import pandas as pd
+import pickle
+
+model = pickle.loads(binary_model)
+
+prediction_label = model.predict(input_tuple_df)[0]
+prediction_score = model.predict_proba(input_tuple_df)[:,1][0]
+
+print(f''Prediction label: {prediction_label}'')
+print(f''Prediction score: {prediction_score}'')
+
+output_data = {
+	''prediction_label'': [prediction_label],
+	''prediction_score'': [prediction_score]
+}
+
+output_df = pd.DataFrame(output_data)'
+
+		, @input_data_1 = @input_query
+		, @input_data_1_name = N'input_tuple_df'
+		, @output_data_1_name = N'output_df'
+		, @params = N'@binary_model VARBINARY(MAX)'
+		, @binary_model = @binary_model
+	WITH RESULT SETS ((
+		  prediction_label INT
+		, prediction_score DECIMAL(5,4)
+	));
+
+END
+GO
+
+
+EXEC stp_predict_titanic_survivors
+	  @ModelName	= 'titanic_flaml'
+	, @ModelVersion	= 1
+
+	, @Age			= 74
+	, @Embarked		= 2.0
+	, @Fare			= 7.775
+	, @Parch		= 0.0
+	, @Pclass		= 1
+	, @Sex			= 1
+	, @SibSp		= 0.0
